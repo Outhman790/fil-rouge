56096d7eddf53a86d8680ec97eec2b5e4983fcd1
Outhman Moumou <tangerino2011@gmail.com>
2025-07-23 21:13:17 +0100

    feat: update payment processing interface

--- pay.php
+++ pay.php
@@ -65,11 +65,10 @@ else :
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="icon" href="assets/img/logo.png" type="image/x-icon">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
-        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <title>Make a payment</title>
         <style>
             .required {
@@ -93,7 +92,7 @@ else :
                                 
                                 <?php if ($payAll) : ?>
                                     <div class="alert alert-info text-center mb-4">
-                                        <i class="fas fa-info-circle mr-2"></i>
+                                        <i class="fas fa-info-circle me-2"></i>
                                         <strong>Bulk Payment:</strong> You are paying for <?php echo count($unpaidMonths) ?> months
                                     </div>
                                 <?php endif; ?>
@@ -122,20 +121,18 @@ else :
                                         <label for="amount">Total Amount <span class="required">*</span></label>
                                         <div class="input-group">
                                             <input type="text" name="amount" id="amount" class="form-control" placeholder="Amount" value="<?php echo $totalAmount; ?>" readonly required />
-                                            <div class="input-group-append">
-                                                <span class="input-group-text">MAD</span>
-                                            </div>
+                                            <span class="input-group-text">MAD</span>
                                         </div>
                                         <?php if ($payAll) : ?>
                                             <small class="form-text text-muted">
-                                                <i class="fas fa-calculator mr-1"></i>
+                                                <i class="fas fa-calculator me-1"></i>
                                                 <?php echo count($unpaidMonths) ?> months × <?php echo $monthlyFee ?> MAD = <?php echo $totalAmount ?> MAD
                                             </small>
                                         <?php endif; ?>
                                     </div>
                                     <button type="submit" name="payNowBtn" class="btn btn-lg btn-primary btn-block">
-                                        <i class="fas fa-credit-card mr-2"></i>Pay <?php echo $totalAmount ?> MAD
-                                        <span class="fa fa-angle-right ml-2"></span>
+                                        <i class="fas fa-credit-card me-2"></i>Pay <?php echo $totalAmount ?> MAD
+                                        <span class="fa fa-angle-right ms-2"></span>
                                     </button>
                                 </form>
                             </div>

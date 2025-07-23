436a1eeb912a234ea3bd0c7b77310dd8d81fc62d
Outhman Moumou <tangerino2011@gmail.com>
2025-07-23 21:13:28 +0100

    refactor: improve page rendering engine

--- render/page-renderer.php
+++ render/page-renderer.php
@@ -24,11 +24,15 @@ class PageRenderer
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Bootstrap CSS (Optimized CDN) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- Shared Variables and Animations -->
+    <link rel="stylesheet" href="css/_variables.css">
+    <link rel="stylesheet" href="css/_animations.css">
     
     <!-- Base Styles -->
     <link rel="stylesheet" href="css/user-dashboard.css">
@@ -53,38 +57,38 @@ class PageRenderer
 
     private static function renderNavigation($currentPage) {
 ?>
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-modern">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="homepage.php">
-                <i class="fas fa-building mr-2"></i>Obuildings
+                <i class="fas fa-building me-2"></i>Obuildings
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item <?php echo $currentPage === 'home' ? 'active' : ''; ?>">
-                        <a class="nav-link" href="homepage.php">
-                            <i class="fas fa-home mr-1"></i>Home
-                            <?php if ($currentPage === 'home') echo '<span class="sr-only">(current)</span>'; ?>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $currentPage === 'home' ? 'active' : ''; ?>" href="homepage.php">
+                            <i class="fas fa-home me-1"></i>Home
+                            <?php if ($currentPage === 'home') echo '<span class="visually-hidden">(current)</span>'; ?>
                         </a>
                     </li>
-                    <li class="nav-item <?php echo $currentPage === 'announces' ? 'active' : ''; ?>">
-                        <a class="nav-link" href="announces.php">
-                            <i class="fas fa-bullhorn mr-1"></i>Announces
-                            <?php if ($currentPage === 'announces') echo '<span class="sr-only">(current)</span>'; ?>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $currentPage === 'announces' ? 'active' : ''; ?>" href="announces.php">
+                            <i class="fas fa-bullhorn me-1"></i>Announces
+                            <?php if ($currentPage === 'announces') echo '<span class="visually-hidden">(current)</span>'; ?>
                         </a>
                     </li>
-                    <li class="nav-item <?php echo $currentPage === 'payments' ? 'active' : ''; ?>">
-                        <a class="nav-link" href="payments.php">
-                            <i class="fas fa-credit-card mr-1"></i>Payments
-                            <?php if ($currentPage === 'payments') echo '<span class="sr-only">(current)</span>'; ?>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $currentPage === 'payments' ? 'active' : ''; ?>" href="payments.php">
+                            <i class="fas fa-credit-card me-1"></i>Payments
+                            <?php if ($currentPage === 'payments') echo '<span class="visually-hidden">(current)</span>'; ?>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="logout.php">
-                            <i class="fas fa-sign-out-alt mr-1"></i>Logout
+                            <i class="fas fa-sign-out-alt me-1"></i>Logout
                         </a>
                     </li>
                 </ul>
@@ -96,8 +100,8 @@ class PageRenderer
 
     public static function renderPageFooter($additionalJS = []) {
 ?>
-    <!-- Bootstrap JS -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Bootstrap JS (Bootstrap 5) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
     <!-- UI Feedback System -->
     <script src="js/ui-feedback.js"></script>

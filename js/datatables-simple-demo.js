2ee42a07b86a74b9f9af488a5b68e277a9b5c4db
Outhman Moumou <tangerino2011@gmail.com>
2025-12-02 17:00:50 +0100

    refactor: update DataTables initialization

--- js/datatables-simple-demo.js
+++ js/datatables-simple-demo.js
@@ -1,9 +1,12 @@
+// Global variable to store DataTable instance
+let residentsDataTable = null;
+
 window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
 
     const datatablesSimple = document.getElementById('datatablesSimple');
     if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+        residentsDataTable = new simpleDatatables.DataTable(datatablesSimple);
     }
 });
